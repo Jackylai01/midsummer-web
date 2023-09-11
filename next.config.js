ee420b06bdc4b15a9cc42f395bf44718d00ad056
jacky.inoby <sn185672@gmail.com>
2023-09-11 11:36:49 +0800

    fix(next.config): add headers

--- next.config.js
+++ next.config.js
@@ -4,4 +4,27 @@ module.exports = {
     defaultLocale: 'zh',
     locales: ['en', 'zh', 'ja', 'ko'],
   },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          { key: 'X-DNS-Prefetch-Control', value: 'on' },
+          { key: 'X-XSS-Protection', value: '1; mode=block' },
+          { key: 'X-Frame-Options', value: 'SAMEORIGIN' },
+          { key: 'X-Content-Type-Options', value: 'nosniff' },
+          { key: 'Referrer-Policy', value: 'origin-when-cross-origin' },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=31536000; includeSubDomains; preload',
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: `script-src 'self' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data: tghtpe.ent.sirsidynix.net; font-src 'self' data:;`,
+          },
+          { key: 'Server', value: '' },
+        ],
+      },
+    ];
+  },
 };
